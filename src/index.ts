9168f28592e0f7df092fbb913e3328102a653e0e
BARTOLOME CANTO MESAS <bartolocan@hotmail.com>
2026-03-02 18:41:15 +0100

    Realizado clonado y borrado

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,35 @@ async function showAllPetsOnConsole(): Promise<void> {
   try {
     const pets: Animal[] = await animalService.getAllPets();
 
-    console.log("Listado completo de pets:");
-    pets.forEach((pet) => {
-      console.log(pet.name, pet.age, pet.species);
-    });
+    const tbody =
+      document.querySelector<HTMLTableSectionElement>("#animal-list")!;
+    const template =
+      document.querySelector<HTMLTemplateElement>("#animal-row-template")!;
 
+    pets.forEach((pet) => {
+      const tr = template.content
+        .querySelector("tr")!
+        .cloneNode(true) as HTMLTableRowElement;
+
+      tr.querySelector(".animal-name")!.textContent = pet.name;
+      tr.querySelector(".animal-species")!.textContent = pet.species;
+      tr.querySelector(".animal-age")!.textContent = String(pet.age);
+
+      // Funcionalidad borrar
+      tr.querySelector<HTMLButtonElement>(".delete-btn")!.addEventListener(
+        "click",
+        async () => {
+          try {
+            await animalService.delete(pet.id); // borra en backend
+            tr.remove(); // borra la fila en la tabla
+          } catch (error) {
+            console.error("Error borrando animal:", error);
+          }
+        }
+      );
+
+      tbody.append(tr);
+    });
   } catch (error) {
     console.error("Error obteniendo pets:", error);
   }
